0758f0cf306d52f566b9ae11201fc7d9593a2df7
ngorbachevski1 <154688442+ngorbachevski1@users.noreply.github.com>
2024-11-26 20:10:04 +0300

    Update Program.cs

--- module_1/Task_levelsInfo/Program.cs
+++ module_1/Task_levelsInfo/Program.cs
@@ -1,4 +1,4 @@
-﻿
+
 using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
@@ -9,53 +9,66 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите верхнюю границу: ");
+            Console.WriteLine("Введите верхнюю границу (только цифры): ");
             decimal levelUp = Convert.ToDecimal(Console.ReadLine());
 
-            Console.WriteLine("Введите нижнюю границу: ");
+            Console.WriteLine("Введите нижнюю границу (только цифры): ");
             decimal levelDown = Convert.ToDecimal(Console.ReadLine());
 
-            Console.WriteLine("Введите шаг уровня: ");
+            Console.WriteLine("Введите шаг уровня (только цифры): ");
             decimal stepSize = Convert.ToDecimal(Console.ReadLine());
 
             // Определяем количество уровней
-            int stepLevels = (int)((levelUp - levelDown) / stepSize) + 1;
-            
-            Console.WriteLine($"\nВаши текущие значения:\n" +
+            try
+            {
+                int stepLevels = (int)((levelUp - levelDown) / stepSize) + 1;
+
+                Console.WriteLine($"\nВаши текущие значения:\n" +
                 $"Верхняя граница = {levelUp}\n" +
                 $"Нижняя граница = {levelDown}\n" +
                 $"Шаг уровня = {stepSize}\n" +
                 $"Предлагаемое количество уровней = {stepLevels}\n");
 
-            
-            Console.WriteLine("\nВведены верные значения? Yes - 1 No - 0");
 
-            int answerData = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\nВведены верные значения? Yes - 1 No - 0");
 
-            var stepLevelsArray = new List<decimal>();
+                int answerData = Convert.ToInt32(Console.ReadLine());
 
-            // Проверка на правильность введенных данных. Согласие пользователя
-            if (answerData == 1)
-            {
-                // Проверка на реальную правильность введенных данных
-                if (levelUp > levelDown + stepSize)
+                var stepLevelsArray = new List<decimal>();
+
+                // Проверка на правильность введенных данных. Согласие пользователя
+                if (answerData == 1)
                 {
-                    for (decimal i = levelDown; i <= levelUp; i = i + stepSize)
+                    // Проверка на реальную правильность введенных данных
+                    if (levelUp > levelDown + stepSize)
                     {
-                        stepLevelsArray.Add(i);
-                        Console.WriteLine($"Уровень: {i}");
+                        int levelNumber = 1;
+                        for (decimal i = levelDown; i <= levelUp; i = i + stepSize)
+                        {
+                            
+                            stepLevelsArray.Add(i);
+                            Console.WriteLine($"Уровень #{levelNumber} : {i}");
+                            levelNumber += 1; 
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Введены неверные данные или невозможно определить уровни");
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Введены неверные данные или невозможно определить уровни");
+                    Console.WriteLine("\nПерезапустите программу и введите правильные значения");
                 }
+                Console.WriteLine($"\nКоличество уровней в массиве = {stepLevelsArray.Count}\n");
             }
-            else
+            catch 
             {
-                Console.WriteLine("\nПерезапустите программу и введите правильные значения");
+                Console.WriteLine("Возникло исключение! Возможно введены неверные данные или вы поделили на ноль");
             }
-            Console.WriteLine($"\nКоличество уровней в массиве = {stepLevelsArray.Count}\n");
+            
+            
+            
         }
     }
 }
